77d9edf94a1e853b2e838abca69a82c051064a03
Josef Utbult <josef@utbult.design>
2023-11-18 21:25:19 +0100

    Render index.html in flask for debugging

--- app.py
+++ app.py
@@ -1,14 +1,14 @@
 from flask import Flask, render_template, request, make_response
 from flask_cors import CORS, cross_origin
 
-app = Flask(__name__)
+app = Flask(__name__, template_folder='./')
 CORS(app)
 app.config['CORS_HEADERS'] = 'Content-Type'
 
 @app.route("/")
 @cross_origin()
 def home():
-    response = make_response(render_template("home.html"))
+    response = make_response(render_template("index.html"))
     response.headers.add("Access-Control-Allow-Origin", "*")
     return response
 
